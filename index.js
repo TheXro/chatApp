c809f374044170f4e7d8dc0c4966f25dc469f26c
TheXro <ravyyadav97@gmail.com>
2023-12-05 23:21:50 +0530

    ejs added

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const express = require("express");
 const app = express();
 const server = http.createServer(app)
 const io = socketio(server)
+
+app.set('view engine', 'ejs');
 app.use('/', express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
@@ -20,18 +22,32 @@ io.on('connection', (socket) => {
     socket.on('msg_send', (data) => {
         console.log('msg received at server', data.msg);
         // socket.emit('msg_rcvd', data) // send to only sender
-        // socket.broadcast.emit('msg_rcvd', data) // send to all except sender
+    //     // socket.broadcast.emit('msg_rcvd', data) // send to all except sender
         io.emit('msg_rcvd', data)
     })
     //rooms
     //using roots people can connect to separate socktes that you specify
     //when people sent msg in room then only person in room will get msg
+    //
+
+
+    //creating room
+
 
 })
 
 // app.listen(3000, () => {
 //     console.log("Server is running on port 3000");
 // }); will give error if try to get socket js  
+
+app.get('/chat/:roomId', (req, res) => {
+    // res.render('index', {
+    //     roomId: req.params.roomId,
+    //     name: 'thexro'
+    // });
+    
+})
+
 
 server.listen(3000, async () => {
     console.log("Server is running on port 3000");
